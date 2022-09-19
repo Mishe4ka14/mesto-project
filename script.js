074d5710953531d0cf29c03c50257655231a862b
Mishe4ka14 <mishechka.14@yandex.ru>
2022-09-19 21:46:43 +0300

    добавил функцию редактирования профиля и добавления новой карточки

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const popup = document.querySelector('#popup');
 const closePopupButton = document.querySelector('#closePopupButton');
 const openPopupButton = document.querySelector('#openPopupButton');
+const container = document.querySelector('.card-container'); //контейнер с карточками
 
 //ниже описываем функции для открытия и закрытия попапа
 function openPopup() {
@@ -24,7 +25,7 @@ openPopupButton?.addEventListener('click', function() {
 });
 
 //ниже все аналогично для модального окна place
-const place = document.querySelector('#place');
+const place = document.querySelector('.place');
 const closePlaceButton = document.querySelector('#closePlaceButton');
 const openPlaceButton = document.querySelector('#openPlaceButton');
 
@@ -46,7 +47,7 @@ openPlaceButton?.addEventListener('click', function() {
 
 
 //ниже описываем функцию для лайка
-let elements = document.querySelector('#elements')
+const elements = document.querySelector('#elements')
 
   elements?.addEventListener('click', Event => {
      const likeButton = Event.target?.closest ('.elements__like-button')
@@ -56,3 +57,42 @@ let elements = document.querySelector('#elements')
 
 
 
+//редактирование имени и информации в профиле
+function popupSubmitHandler (evt) {
+  evt.preventDefault(); //отмена стандартной отправки формы
+
+  // Получаем значение полей jobInput и nameInput из свойства value
+  const nameInput = document.querySelector('.popup__field_name');
+  const jobInput = document.querySelector('.popup__field_job');
+
+  //Выбераем элементы, куда должны быть вставлены значения полей
+  let profileTitle = document.querySelector('.profile__title');
+  let profileSubtitle = document.querySelector('.profile__subtitle');
+
+  //Вставляем новые значения с помощью textContent
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+  closePopup();
+}
+//Прикрепляем обработчик к форме
+popup.addEventListener('submit', popupSubmitHandler);
+
+//ниже функция добавления карточки
+function addCard(placeTitle, placeLink) { //функция с параметрами названия карточки и ссылкой
+  const cardTemplate = document.querySelector('.template').content; //создаем переменную для шаблона
+  const cardElement = cardTemplate.querySelector('.template__element').cloneNode(true); //создаем клон шаблона, в который будем подставлять значения
+
+  placeTitle = document.querySelector('.place__name'); //присваиваем параметрам функции название и ссылку из модального окна
+  placeLink = document.querySelector('.place__link');
+
+  cardElement.querySelector('.elements__photo-title').textContent = placeTitle.value; //подставляем значения в шаблон
+  cardElement.querySelector('.elements__photo').src = placeLink.value;
+
+  container.prepend(cardElement); //вставляем разметку карточки в DOM
+}
+
+place.addEventListener('submit', function(e) { //обработчик событий ко всему модальному окну
+  e.preventDefault();
+  addCard();
+});
